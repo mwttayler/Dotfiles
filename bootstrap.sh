00d22d89a71280b5c89680d1a34a659df9ef6a6d
Max Tayler <maxtayler1@gmail.com>
2022-03-11 14:04:28 +0000

    Update bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,10 @@
 #!/bin/zsh
 
+# Install Homebrew
 /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install Stow
+brew install stow
